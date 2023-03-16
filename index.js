8c90ad9fab5e34a2c3629b226dfe922b28a8abdf
Kocaklejdi <kocaklejdi@gmail.com>
2023-03-16 19:47:39 +0100

    Revert "school frontend"
    This reverts commit 2238623149929b650317303c24510b054079ad19.

--- index.js
+++ index.js
@@ -1,114 +1,91 @@
-  const form = document.getElementById("form");
-
-  const formCheckboxes = document.getElementById("formCheckBoxes");
-
-
-
-
-
-  form.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    const formCourseInput = document.getElementById("formCourseInput");
-    const checkboxes = document.getElementsByClassName("check");
-    const checkedCheckboxes = []
-    for(let i = 0; i<checkboxes.length;i++){
-      if(checkboxes[i].checked){
-        checkedCheckboxes.push(parseInt(checkboxes[i].value));
-      }
-    }
-    const obj = {courseName:formCourseInput.value, students:checkedCheckboxes };
-    console.log(JSON.stringify(obj))
-    const options = {
-      headers: { "Content-Type": "application/json"},
-      method: "POST", 
-      body: JSON.stringify(obj),
-  }
-    fetch("http://localhost:3000/addNewCourse",options).then((response) => response.json()).then((data) => {
-      fetchCourses();
-    })
-    //console.log({courseName:formCourseInput.value, students:checkedCheckboxes })
-  })
-function makeStudentCheckboxes(students){
-  const studentCheckboxes = students.map((student)=>{
-    const studentDiv = document.createElement("div");
-    const nameLabel = document.createElement("label");
-    nameLabel.innerText = student.name;
-    const studentCheckbox = document.createElement("input");
-    studentCheckbox.type = "checkbox";
-    studentCheckbox.value = student.id;
-    studentCheckbox.className = "check";
-    studentDiv.appendChild(nameLabel);
-    studentDiv.appendChild(studentCheckbox);
-    return studentDiv
-  })
-  formCheckboxes.replaceChildren(...studentCheckboxes)
-}
-
-  const button = document.getElementById("createCourseButton");
-  const formContainer = document.getElementById("formContainer");
-  let bool = false;
-  button.onclick = () =>{
-    if(bool){
-      formContainer.style.visibility = "hidden"
-      bool = !bool;
-    } else{
-      fetchStudentsAndMakeCheckboxes();
-      formContainer.style.visibility = "visible"
-      bool = !bool;
-    }
-  }
-fetchCourses();
-function fetchCourses(){
-  fetch("http://localhost:3000/courses").then((val)=>val.json()).then((course)=>{
-    const courseDivs = course.map((element)=>{
-      const course = document.createElement("div");
-      course.style = `
-      width: 200px;
-      height: 200px;
-      border: 2px solid red;
-      border-radius: 32px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      `
-      course.onclick = () => {
-          
-          //console.log(element.name)
-          localStorage.setItem("courseId",element.id.toString());
-          //console.log(element.id)
-          window.location.href='course.html';
-      
-      }
-      course.innerText = element.name;
-      return course;
-    })
-    courseContainer.replaceChildren(...courseDivs)
-  })
-}
-function getStudentByID(id){
-  const obj = {id: id};
-  const options ={
-    headers: { "Content-Type": "application/json"},
-    method: "POST", 
-    body: JSON.stringify(obj),
-}
-  fetch("http://localhost:3000/students",options).then((val)=>val.json()).then((data)=>console.log(data));
-}
-
-function getCourseByID(id){
-  const obj = {id: id};
-  const options ={
-    headers: { "Content-Type": "application/json"},
-    method: "POST", 
-    body: JSON.stringify(obj),
-}
-  fetch("http://localhost:3000/courses",options).then((val)=>val.json()).then((data)=>console.log(data));
-}
-
-function fetchStudentsAndMakeCheckboxes(){
-fetch("http://localhost:3000/students").then((val)=>val.json()).then((students)=>{
-  makeStudentCheckboxes(students);
-});
-};
-const courseContainer = document.getElementById("courses");
+const students = [
+    {
+      name: "Test name 1",
+      id: 1,
+    },
+    {
+      name: "Test name 2",
+      id: 2,
+    },
+    {
+      name: "Test name 3",
+      id: 3,
+    },
+    {
+      name: "Test name 4",
+      id: 4,
+    },
+    {
+      name: "Test name 5",
+      id: 5,
+    },
+  ];
   
+  const courses = [
+    {
+      id: 1,
+      name: "frontend",
+      students: [
+        {
+          id: 1,
+          grades: [10, 9, 8, 10],
+        },
+        {
+          id: 2,
+          grades: [7, 9, 8, 10],
+        },
+        {
+          id: 3,
+          grades: [10, 9, 8, 4],
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: "socialMedia",
+      students: [
+        {
+          id: 5,
+          grades: [6, 7, 8],
+        },
+        {
+          id: 1,
+          grades: [],
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: "graphicDesign",
+      students: [
+        { id: 3, grades: [4, 4] },
+        { id: 5, grades: [] },
+        { id: 2, grades: [6, 6, 6] },
+      ],
+    },
+  ];
+  const courseContainer = document.getElementById("courses");
+  const courseDivs = courses.map((element)=>{
+    const course = document.createElement("div");
+    course.style = `
+    width: 200px;
+    height: 200px;
+    border: 2px solid red;
+    border-radius: 32px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    `
+    course.onclick = () => {
+        
+        //console.log(element.name)
+        localStorage.setItem("courseId",element.id.toString());
+        //console.log(element.id)
+        window.location.href='course.html';
+    
+    }
+    course.innerText = element.name;
+    return course;
+  })
+  courseContainer.append(...courseDivs)
+  console.log(courseDivs);
